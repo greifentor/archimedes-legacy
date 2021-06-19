f6216ea92a96bec7528b6f869e49ae8d25335c07
ollie <lieshoff@gmx.de>
2021-06-19 22:12:48 +0200

    Generalization of the script generator.

--- legacy-core/src/main/java/archimedes/legacy/exporter/LiquibaseScriptCreator.java
+++ legacy-core/src/main/java/archimedes/legacy/exporter/LiquibaseScriptCreator.java
@@ -5,106 +5,28 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Vector;
 
-import archimedes.gui.DatabaseConnectionRecord;
-import archimedes.legacy.gui.ExceptionDialog;
-import archimedes.legacy.gui.ModelReaderProgressMonitor;
-import archimedes.legacy.gui.connections.ConnectFrame;
-import archimedes.legacy.importer.jdbc.JDBCImportConnectionData;
-import archimedes.legacy.importer.jdbc.JDBCImportManager;
-import archimedes.legacy.importer.jdbc.JDBCImportManagerConfigurationDialog;
-import archimedes.legacy.model.DiagrammModel;
-import archimedes.legacy.scheme.Diagramm;
-import archimedes.legacy.updater.DataModelToCMOConverter;
-import baccara.gui.GUIBundle;
-import baccara.gui.generics.EditorFrameEvent;
-import baccara.gui.generics.EditorFrameEventType;
-import baccara.gui.generics.EditorFrameListener;
-import corent.gui.DefaultFrameTextViewerComponentFactory;
-import corent.gui.FrameTextViewer;
-import de.ollie.dbcomp.comparator.DataModelComparator;
 import de.ollie.dbcomp.comparator.model.ChangeActionCRO;
 import de.ollie.dbcomp.liquibase.writer.ChangeActionToDatabaseChangeLogConverter;
 import liquibase.changelog.DatabaseChangeLog;
 import liquibase.serializer.core.xml.XMLChangeLogSerializer;
-import logging.Logger;
 
 /**
  * A class which is able to create a liquibase script to update a database based on a diagram.
  *
  * @author ollie (24.02.2021)
  */
-public class LiquibaseScriptCreator {
+public class LiquibaseScriptCreator extends ScriptCreator {
 
-	private static final Logger LOG = Logger.getLogger(LiquibaseScriptCreator.class);
+	@Override
+	protected List<String> createScript(List<ChangeActionCRO> changeActions) {
+		DatabaseChangeLog databaseChangeLog = new ChangeActionToDatabaseChangeLogConverter().convert(changeActions);
+		return Arrays.asList(exportDatabaseChangeLog(databaseChangeLog).toString());
+	}
 
-	public void createScript(DiagrammModel diagramm, GUIBundle guiBundle) {
-		JDBCImportConnectionData connectionData =
-				new JDBCImportConnectionData().setConnections(diagramm.getDatabaseConnections());
-		JDBCImportManagerConfigurationDialog connectionDialog =
-				new JDBCImportManagerConfigurationDialog(connectionData, guiBundle);
-		connectionDialog.setVisible(true);
-		connectionDialog
-				.addEditorFrameListener(
-						new EditorFrameListener<EditorFrameEvent<DatabaseConnectionRecord, ConnectFrame>>() {
-							@Override
-							public void eventFired(
-									final EditorFrameEvent<DatabaseConnectionRecord, ConnectFrame> event) {
-								if (event.getEventType() == EditorFrameEventType.OK) {
-									final Thread t = new Thread(() -> {
-										ModelReaderProgressMonitor mrpm = new ModelReaderProgressMonitor(guiBundle, 5);
-										try {
-											Diagramm d = (Diagramm) new JDBCImportManager()
-													.importDiagram(connectionData, mrpm::update);
-											mrpm.enableCloseButton();
-											if (d != null) {
-												DataModelToCMOConverter converter = new DataModelToCMOConverter();
-												DataModelComparator comparator = new DataModelComparator();
-												List<ChangeActionCRO> changeActions = comparator
-														.compare(
-																converter
-																		.convert(
-																				diagramm,
-																				table -> table
-																						.getOptionByName(
-																								"NO_DB") != null),
-																converter.convert(d))
-														.getChangeActions();
-												DatabaseChangeLog databaseChangeLog =
-														new ChangeActionToDatabaseChangeLogConverter()
-																.convert(changeActions);
-												new FrameTextViewer(
-														new Vector<String>(
-																Arrays
-																		.asList(
-																				exportDatabaseChangeLog(
-																						databaseChangeLog).toString())),
-														DefaultFrameTextViewerComponentFactory.INSTANCE,
-														guiBundle.getInifile(),
-														"Liquibase Update Script",
-														"");
-											}
-										} catch (Exception e) {
-											mrpm.setVisible(false);
-											LOG
-													.error(
-															"error detected while importing from JDBC connection: "
-																	+ e.getMessage());
-											new ExceptionDialog(
-													e,
-													guiBundle
-															.getResourceText(
-																	"Exception.ImportModel.text",
-																	e.getMessage()),
-													guiBundle);
-										}
-									});
-									t.start();
-								}
-							}
-						});
-
+	@Override
+	protected String getViewerTitel() {
+		return "Liquibase Update Script";
 	}
 
 	private OutputStream exportDatabaseChangeLog(DatabaseChangeLog databaseChangeLog) {
---
+++ legacy-core/src/main/java/archimedes/legacy/exporter/ScriptCreator.java
@@ -0,0 +1,100 @@
+package archimedes.legacy.exporter;
+
+import java.util.List;
+import java.util.Vector;
+
+import archimedes.gui.DatabaseConnectionRecord;
+import archimedes.legacy.gui.ExceptionDialog;
+import archimedes.legacy.gui.ModelReaderProgressMonitor;
+import archimedes.legacy.gui.connections.ConnectFrame;
+import archimedes.legacy.importer.jdbc.JDBCImportConnectionData;
+import archimedes.legacy.importer.jdbc.JDBCImportManager;
+import archimedes.legacy.importer.jdbc.JDBCImportManagerConfigurationDialog;
+import archimedes.legacy.model.DiagrammModel;
+import archimedes.legacy.scheme.Diagramm;
+import archimedes.legacy.updater.DataModelToCMOConverter;
+import baccara.gui.GUIBundle;
+import baccara.gui.generics.EditorFrameEvent;
+import baccara.gui.generics.EditorFrameEventType;
+import baccara.gui.generics.EditorFrameListener;
+import corent.gui.DefaultFrameTextViewerComponentFactory;
+import corent.gui.FrameTextViewer;
+import de.ollie.dbcomp.comparator.DataModelComparator;
+import de.ollie.dbcomp.comparator.model.ChangeActionCRO;
+import logging.Logger;
+
+/**
+ * A class which is able to create a liquibase script to update a database based on a diagram.
+ *
+ * @author ollie (24.02.2021)
+ */
+public abstract class ScriptCreator {
+
+	private static final Logger LOG = Logger.getLogger(ScriptCreator.class);
+
+	public void createScript(DiagrammModel diagramm, GUIBundle guiBundle) {
+		JDBCImportConnectionData connectionData =
+				new JDBCImportConnectionData().setConnections(diagramm.getDatabaseConnections());
+		JDBCImportManagerConfigurationDialog connectionDialog =
+				new JDBCImportManagerConfigurationDialog(connectionData, guiBundle);
+		connectionDialog.setVisible(true);
+		connectionDialog
+				.addEditorFrameListener(
+						new EditorFrameListener<EditorFrameEvent<DatabaseConnectionRecord, ConnectFrame>>() {
+							@Override
+							public void eventFired(
+									final EditorFrameEvent<DatabaseConnectionRecord, ConnectFrame> event) {
+								if (event.getEventType() == EditorFrameEventType.OK) {
+									final Thread t = new Thread(() -> {
+										ModelReaderProgressMonitor mrpm = new ModelReaderProgressMonitor(guiBundle, 5);
+										try {
+											Diagramm d = (Diagramm) new JDBCImportManager()
+													.importDiagram(connectionData, mrpm::update);
+											mrpm.enableCloseButton();
+											if (d != null) {
+												DataModelToCMOConverter converter = new DataModelToCMOConverter();
+												DataModelComparator comparator = new DataModelComparator();
+												List<ChangeActionCRO> changeActions = comparator
+														.compare(
+																converter
+																		.convert(
+																				diagramm,
+																				table -> table
+																						.getOptionByName(
+																								"NO_DB") != null),
+																converter.convert(d))
+														.getChangeActions();
+												new FrameTextViewer(
+														new Vector<String>(createScript(changeActions)),
+														DefaultFrameTextViewerComponentFactory.INSTANCE,
+														guiBundle.getInifile(),
+														getViewerTitel(),
+														"");
+											}
+										} catch (Exception e) {
+											mrpm.setVisible(false);
+											LOG
+													.error(
+															"error detected while importing from JDBC connection: "
+																	+ e.getMessage());
+											new ExceptionDialog(
+													e,
+													guiBundle
+															.getResourceText(
+																	"Exception.ImportModel.text",
+																	e.getMessage()),
+													guiBundle);
+										}
+									});
+									t.start();
+								}
+							}
+						});
+
+	}
+
+	protected abstract List<String> createScript(List<ChangeActionCRO> changeActions);
+
+	protected abstract String getViewerTitel();
+
+}
