cd7c7d2ef5c8e8cd9dcd2da499133a681f195727
ollie <lieshoff@gmx.de>
2021-06-29 23:48:14 +0200

    Updated version in the start files.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 LIB=./legacy-core/lib
 
-CP=./legacy-core/target/archimedes-legacy-core-2.12.0-executable.jar
+CP=./legacy-core/target/archimedes-legacy-core-2.14.0-executable.jar
 
 CP=$CP:$LIB/baccaraacf.jar
 CP=$CP:$LIB/commons-lang3-3.1.jar
