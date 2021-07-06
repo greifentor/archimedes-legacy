71310de4d766d5da40331cd1d8a5be175d32ac90
Oliver Lieshoff <o.lieshoff@schuetze-consulting.ag>
2021-07-06 08:22:23 +0200

    Made new version script working for Windows.

--- new-version.bat
+++ new-version.bat
@@ -1,3 +1,4 @@
+@ECHO OFF
 ECHO Setting Archimedes project version to: %1
 
 CALL mvn versions:set "-DnewVersion=%1"
@@ -5,5 +6,5 @@ CALL mvn versions:set "-DnewVersion=%1"
 
 CALL mvn clean install -Dmaven.test.skip=true
 
-java -cp legacy-core\target\archimedes-legacy-core-%ARCHIMEDES_VERSION%.jar archimedes.util.VersionBatchWriter . %1 LINUX
-java -cp legacy-core\target\archimedes-legacy-core-%ARCHIMEDES_VERSION%.jar archimedes.util.VersionBatchWriter . %1 WINDOWS
+java -cp legacy-core\target\archimedes-legacy-core-%1.jar archimedes.util.VersionBatchWriter . %1 LINUX
+java -cp legacy-core\target\archimedes-legacy-core-%1.jar archimedes.util.VersionBatchWriter . %1 WINDOWS
